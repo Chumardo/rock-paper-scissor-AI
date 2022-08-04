e16f022574e141c3fe1a57f0364661a36eee1a8d
Chumardo <dachigiorgadze98@gmail.com>
2022-08-04 22:11:28 +0400

    creating interface

--- main.py
+++ main.py
@@ -3,8 +3,19 @@ import cvzone
 
 
 cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
 
 while True:
+    imgBG = cv2.imread("Resources/BG.png")
     success, img = cap.read()
+
+    imgScaled = cv2.resize(img,(0,0),None,0.875,0.875)
+    imgScaled = imgScaled[:,80:480]
+    
+    imgBG[234:654,795:1195] = imgScaled
+
     cv2.imshow('Image',img)
+    cv2.imshow("BG", imgBG)
+    
     cv2.waitKey(1)
