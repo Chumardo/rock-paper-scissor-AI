9369f7d11102bf902832b6464c5d22d591e8572f
Chumardo <dachigiorgadze98@gmail.com>
2022-08-05 09:11:53 +0400

    detecting hand

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import cv2
 import cvzone
-
+from cvzone.HandTrackingModule import HandDetector
 
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
+
+
+detector = HandDetector(maxHands=1)
 
 while True:
     imgBG = cv2.imread("Resources/BG.png")
@@ -13,9 +16,12 @@ while True:
     imgScaled = cv2.resize(img,(0,0),None,0.875,0.875)
     imgScaled = imgScaled[:,80:480]
     
+    hands, img = detector.findHands(imgScaled)
+    
     imgBG[234:654,795:1195] = imgScaled
 
-    cv2.imshow('Image',img)
+    
+    
     cv2.imshow("BG", imgBG)
     
     cv2.waitKey(1)
